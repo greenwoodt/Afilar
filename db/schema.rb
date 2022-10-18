5294c892d2176550551d80824e10a329249c9411
Tom Greenwood <tom.t.greenwood@gmail.com>
2022-10-18 22:14:50 +0200

    review model created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_200830) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_201348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_200830) do
     t.string "address"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "comment"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -60,4 +69,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_200830) do
   end
 
   add_foreign_key "bookings", "users", column: "users_id"
+  add_foreign_key "reviews", "bookings"
 end
